65e56b91d72587795d9bbd7da625e514338a162f
Martin Mirda <martin.mirda@kosickaakademia.sk>
2025-06-18 17:02:32 +0200

    refactor: remove unused imports in CameraController

--- src/main/java/sk/kas/mirda/Cameras/CameraController.java
+++ src/main/java/sk/kas/mirda/Cameras/CameraController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sk.kas.mirda.Cameras.model.Camera;
-import sk.kas.mirda.Cameras.model.Lens;
 import sk.kas.mirda.Cameras.repository.CameraRepository;
-import sk.kas.mirda.Cameras.repository.LensRepository;
 import java.util.List;
 import java.util.Optional;
 
